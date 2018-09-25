fe917bb48b95b05ab8f375d45377feef5fb08847
Adam Pearce <1wheel@gmail.com>
2018-09-25 17:12:42 -0400

    tidy

--- 00-arrows/_script.js
+++ 00-arrows/_script.js
@@ -10,65 +10,46 @@ d3.loadData(`../raw-data/${times[0]}.json`, 'earth.json', (err, res) => {
   var c = d3.conventions({
     sel, 
     margin: {top: 0, bottom: 0, left: 0, right: 0},
-    layers: 'sccc'
+    layers: 'scc'
   })
 
-  ;[svg, ctxBot, ctx0, ctx1] = c.layers
+  ;[svg, ctx0, ctxBot] = c.layers
   var {width, height} = c
 
-  // projection
-  !(function(){
-    // var extent = [[-83.028,32.2],[-75.59,36.2]]
-    extent = [[360 -83.028,32.2],[360 -75.59,46.2]]
-    proj = d3.geoTransverseMercator().rotate([-84, 0, -170])
-      .fitSize([c.width, c.height], {type: "MultiPoint", coordinates: extent})    
+  extent = [[360 -83.028,28.2],[360 -75.59,46.2]]
+  proj = d3.geoTransverseMercator().rotate([-84, 0, -170])
+    .fitSize([c.width, c.height], {type: "MultiPoint", coordinates: extent})    
 
-    extent = [[360 -124.848974, 24.396308],[360 -63.885444, 49.384358]]
-    proj = d3.geoAlbers()
-      .fitSize([c.width, c.height], {type: "MultiPoint", coordinates: extent})    
-
-    // extent = [[0, -90],[360, 90]]
-    // proj = d3.geoEquirectangular()
-    //   .fitSize([c.width, c.height], {type: "MultiPoint", coordinates: extent})
-
-    extent = [[0, c.height], [c.width, 0]].map(proj.invert)
-    extent[0][0] += 360    
-    extent[1][0] += 360  
-  })()
+  extent = [[0, c.height], [c.width, 0]]
+    .map(proj.invert)
+    .map(d => { d[0]+= 360; return d })
 
   var path = d3.geoPath().context(ctx0).projection(proj)
   path(topojson.mesh(earth, earth.objects.coastline_50m))
-  ctx0.strokeStyle = "rgba(255,0,0,.4)"
+  ctx0.strokeStyle = "rgba(255,255,0,.4)"
   ctx0.stroke()
 
   // rounded extent
   ;[[x0, y0], [x1, y1]] = extent
     .map((d, i) => d.map(i ? Math.ceil : Math.floor).map(d => i ? d + 5 : d - 5))
 
-  var nex = x1 - x0
-  var ney = y1 - y0
+  var nx = x1 - x0
+  var ny = y1 - y0
   lnglat2uv = {}
-  points = d3.range(0, nex*ney).map(i => {
-    var lng = x0 + (i % nex)
-    var lat = y0 + Math.floor(i/nex)
-
-    // var index = (lng + 180) + (lat + 90)*360
+  points = d3.range(0, nx*ny).map(i => {
+    var lng = x0 + (i % nx)
+    var lat = y0 + Math.floor(i/nx)
     var index = lng + (90 - lat)*360
 
     var u = grib[0].data[index]
     var v = grib[1].data[index]
 
     var pos = proj([lng, lat])
-
     var rv = {lng, lat, u, v, pos}
 
     lnglat2uv[lng + '' + lat] = rv
-
     return rv
   })
-
-  console.log(d3.extent(points, d => d.lng))
-  console.log(d3.extent(points, d => d.lat))
 
   var s = 1
   grid = d3.range(width*height/s/s).map(i => {
@@ -86,13 +67,9 @@ d3.loadData(`../raw-data/${times[0]}.json`, 'earth.json', (err, res) => {
     var x1y1 = lnglat2uv[(rlng + 1) + '' + (rlat + 1)]
     var x0y1 = lnglat2uv[(rlng + 0) + '' + (rlat + 1)]
 
-    if (i == 1200){
-      console.log({rlng, rlat})
-      console.log(rlng +''+ rlat)
-    }
-
     if (!x0y0 || !x1y0 || !x1y1 || !x0y1) return {px, py, lng, lat, u: 0, v: 0}
 
+    // https://en.wikipedia.org/wiki/Bilinear_interpolation
     var tx = lng - rlng
     var ty = lat - rlat
     var u = x0y0.u*(1 - tx)*(1 - ty)
@@ -108,75 +85,43 @@ d3.loadData(`../raw-data/${times[0]}.json`, 'earth.json', (err, res) => {
     return {px, py, lng, lat, u, v}
   })
 
+  dots = d3.range(5000).map(d => makePoint({}))
 
-  svg.appendMany('g', points)
-    .st({opacity: .5})
-    .translate(d => proj([d.lng, d.lat]))
-    .append('circle')
-    .at({
-      r: 2,
-      fill: 'none',
-      strokeWidth: .5,
-      stroke: '#f0f',
-    })
-    .parent()
-    .append('path')
-    .at({
-      d: d => 'M 0 0 L' + [d.u*.8, -d.v*.8],
-      stroke: '#f0f'
-    })
+  function makePoint(d){
+    d.px = Math.random()*width
+    d.py = Math.random()*height
+    d.u = 0 
+    d.v = 0
+    d.age = d.age ? d.age - 100 : Math.random()*100
 
-  // ctx1.beginPath()
-  // grid.forEach(d => {
-  //   ctx1.moveTo(d.px, d.py)
-  //   ctx1.lineTo(d.px + d.u*.5, d.py + d.v*.5)
-  // })
-  // ctx1.strokeStyle = 'rgba(255,0,255,.5)'
-  // ctx1.strokeStyle = '#f0f'
-  // ctx1.stroke()
+    return d
+  }
 
-
-  dots = d3.range(5000).map(d => {
-    var px = Math.random()*width
-    var py = Math.random()*height
-    var age = Math.random()*100
-
-    return {px, py, age}
-  })
-
+  var xOffset = 0
   if (window.timer) window.timer.stop()
-
   window.timer = d3.timer(t => {
+    xOffset += 0
     dots.forEach(d => {
-      var px = Math.round(d.px)
+      var px = Math.round(d.px + xOffset) % width
       var py = Math.round(d.py)
 
       var v = grid[px + py*width]
-
-      d.age++
-      var inBounds = 0 <= d.px && d.px < width && 0 <= d.py && d.py < height
-      if (!v || (v.v == 0 && v.u == 0) || !inBounds || d.age > 100){
-        d.px = Math.random()*width
-        d.py = Math.random()*height
-        d.u = 0 
-        d.v = 0
-        d.age = 0
-      } else {
-        d.u = v.u/5
-        d.v = v.v/5
-
-        d.px += d.u
-        d.py += -d.v
-      }
+      if (!v || d.age++ > 100) return makePoint(d)
+        
+      d.u = v.u/5
+      d.v = v.v/5
+      d.px += d.u
+      d.py += -d.v
+      d.mag = Math.sqrt(d.u*d.u + d.v*d.v)
     })
 
     ctxBot.globalCompositeOperation = 'destination-in'
-    ctxBot.fillStyle = 'rgba(0, 0, 0, 0.95)'
+    ctxBot.fillStyle = 'rgba(0, 0, 0, 0.97)'
     ctxBot.fillRect(0, 0, width, height)
     ctxBot.globalCompositeOperation = 'source-over'
 
-    var opacityScale = d3.scaleLinear().domain([0, 10]).range([.3, 1])
-    d3.nestBy(dots, d => Math.round(d.u + d.v)).forEach(bucket => {
+    var opacityScale = d3.scaleLinear().domain([0, 10]).range([.15, 1])
+    d3.nestBy(dots, d => Math.round(d.mag)).forEach(bucket => {
       ctxBot.beginPath()
       bucket.forEach(d => {
         ctxBot.moveTo(d.px, d.py)
@@ -184,9 +129,32 @@ d3.loadData(`../raw-data/${times[0]}.json`, 'earth.json', (err, res) => {
       })
       ctxBot.strokeStyle = `rgba(255,255,255,${opacityScale(bucket.key)})`
       ctxBot.stroke()
-      
     })
 
   })
+
+
+
+
+
+
+
+  // svg.appendMany('g', points)
+  //   .st({opacity: .5})
+  //   .translate(d => proj([d.lng, d.lat]))
+  //   .append('circle')
+  //   .at({
+  //     r: 2,
+  //     fill: 'none',
+  //     strokeWidth: .5,
+  //     stroke: '#f0f',
+  //   })
+  //   .parent()
+  //   .append('path')
+  //   .at({
+  //     d: d => 'M 0 0 L' + [d.u*.8, -d.v*.8],
+  //     stroke: '#f0f'
+  //   })
+
 
 })
