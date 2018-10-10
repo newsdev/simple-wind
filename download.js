b0d66992e941f0ac16ee91f3cc1ae388afa0644d
kklai <kkrebeccalai@gmail.com>
2018-10-10 11:25:22 -0400

    loll

--- download.js
+++ download.js
@@ -7,7 +7,7 @@ curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t06z.pgrb2
 ../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100600.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100600
 
 curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t06z.pgrb2.1p00.f003&lev_10_m_above_ground=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.2018101006" -o raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100600f3
-../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100600.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100600f3
+../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100600f3.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100600f3
 
 
 
@@ -15,7 +15,7 @@ curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t06z.pgrb2
 ../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100606.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100606
 
 curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t06z.pgrb2.1p00.f003&lev_10_m_above_ground=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.2018101006" -o raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100606f3
-../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100606.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100606f3
+../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100606f3.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100606f3
 
 
 
@@ -24,7 +24,7 @@ curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t06z.pgrb2
 ../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100612.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100612
 
 curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t06z.pgrb2.1p00.f003&lev_10_m_above_ground=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.2018101006" -o raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100612f3
-../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100612.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100612f3
+../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100612f3.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100612f3
 
 
 
@@ -33,7 +33,7 @@ curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t06z.pgrb2
 ../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100618.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100618
 
 curl "http://nomads.ncep.noaa.gov/cgi-bin/filter_gfs_1p00.pl?file=gfs.t06z.pgrb2.1p00.f003&lev_10_m_above_ground=on&leftlon=0&rightlon=360&toplat=90&bottomlat=-90&dir=%2Fgfs.2018101006" -o raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100618f3
-../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100618.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100618f3
+../grib2json/target/grib2json-0.8.0-SNAPSHOT/bin/grib2json -d -n -o raw-data-f/2018100618f3.json raw-data-f/gfs.t00z.pgrb2.1p00.f000.2018100618f3
 
 
 `
