25bbbb8111458c19d2176a1bc03431f2636833e9
Adam Pearce <1wheel@gmail.com>
2018-10-20 01:16:22 -0400

    more data

--- download.js
+++ download.js
@@ -4,10 +4,18 @@ var fs = require('fs')
 
 var cmdStr = fs.readFileSync(__dirname + '/download.sh', 'utf8')
 
-execSync(cmdStr.replace(/20181006/g, '20181007'))
-execSync(cmdStr.replace(/20181006/g, '20181008'))
-execSync(cmdStr.replace(/20181006/g, '20181009'))
-execSync(cmdStr.replace(/20181006/g, '20181010'))
+// execSync(cmdStr.replace(/20181006/g, '20181007'))
+// execSync(cmdStr.replace(/20181006/g, '20181008'))
+// execSync(cmdStr.replace(/20181006/g, '20181009'))
+// execSync(cmdStr.replace(/20181006/g, '20181010'))
+
+execSync(cmdStr.replace(/20181006/g, '20181011'))
+execSync(cmdStr.replace(/20181006/g, '20181012'))
+execSync(cmdStr.replace(/20181006/g, '20181013'))
+execSync(cmdStr.replace(/20181006/g, '20181014'))
+execSync(cmdStr.replace(/20181006/g, '20181015'))
+execSync(cmdStr.replace(/20181006/g, '20181016'))
+
 
 
 
